a26b9edabdfe889dbb39b6f80a089fcc2fd41096
sreevallisarvasuddi <sreevalli0212@gmail.com>
2026-09-22 15:38:52 +0530

    Time: 0 ms (100%), Space: 8.1 MB (59.02%) - LeetHub

---
+++ 0065-valid-number/0065-valid-number.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isNumber(string s) {
+        if(s[0]=='+'|| s[0]=='-') s.erase(s.begin());
+        int i=0;
+        int dot=0;
+        while(i<s.size()) {
+            if(s[i]=='e' || s[i]=='E') {
+                i++;
+                break;
+            }
+            else if(s[i]=='.') {
+                if(dot) return 0;
+                if((i>0 && (s[i-1]>='0' && s[i-1]<='9')) || (i<s.size()-1 && (s[i+1]>='0' && s[i+1]<='9'))) i++;
+                else return 0;
+                dot++;
+            }
+            else if(s[i]>='0' && s[i]<='9') i++;
+            else return 0;
+        } 
+        if(i<s.size() && (s[i]=='+'|| s[i]=='-')) {
+            if(i>0 && (s[i-1]=='e' || s[i-1]=='E')) {
+                if(((i>=2 && s[i-2]>='0' && s[i-2]<='9') || (i>=3 && s[i-3]>='0' && s[i-3]<='9' && s[i-2]=='.')) && (i<s.size()-1 && s[i+1]>='0' && s[i+1]<='9'));
+                
+                else return 0;
+            }
+            i++;
+        }
+        else if(i>0 && (s[i-1]=='e' || s[i-1]=='E')) {
+            if(((i>=2 && s[i-2]>='0' && s[i-2]<='9') || (i>=3 && s[i-3]>='0' && s[i-3]<='9' && s[i-2]=='.')) && (i<s.size() && s[i]>='0' && s[i]<='9'));
+            else return 0;
+        }
+        while(i<s.size()) {
+            if(s[i]>='0' && s[i]<='9') i++;
+            else return 0;
+        }
+        if(!s.empty()) return 1;
+        return 0;
+    }
+};
